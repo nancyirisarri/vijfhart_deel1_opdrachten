0c17f4fce97d0a6887f71140a5d0b195382b3453
nancyirisarri <nancyirisarri@gmail.com>
2017-04-05 22:40:37 +0200

    Improve indenting.

--- hoofdstuk5/CharApp.java
+++ hoofdstuk5/CharApp.java
@@ -1,24 +1,25 @@
 class CharApp {
 	public static void main(String args[]) {
 
-      System.out.printf("    ");
-  	  
-  	  for(int j = 0; j<=9; j++) {
-        System.out.printf(j + " ");	
-      }
-		
-	  System.out.println();	
-	  
-      for(int i = 30; i<=129; i+=10) {
-        
-        if(i<100) System.out.printf(" ");
-        System.out.printf("%s ", i);
-      
-        for(int j = 0; j<=9; j++) {
-	      System.out.printf("%s ", (char)(j+i));	
-	    }
-	    
-	    System.out.println();
-	  }
-    }
+		System.out.printf("    ");
+
+		for (int j = 0; j <= 9; j++) {
+			System.out.printf(j + " ");
+		}
+
+		System.out.println();
+
+		for (int i = 30; i <= 129; i += 10) {
+
+			if (i < 100)
+				System.out.printf(" ");
+			System.out.printf("%s ", i);
+
+			for (int j = 0; j <= 9; j++) {
+				System.out.printf("%s ", (char) (j + i));
+			}
+
+			System.out.println();
+		}
+	}
 }
